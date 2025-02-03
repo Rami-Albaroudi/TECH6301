4e19a1649063264916d9b8d09a4438508abf9f4c
Rami <99697671+Rami-Albaroudi@users.noreply.github.com>
2025-02-03 15:54:33 -0500

    123

--- PythonBasics/Calculator/main.py
+++ PythonBasics/Calculator/main.py
@@ -1,1 +1,12 @@
-print("hi")
+first_number = int(input("Enter a number: "))
+second_number = int(input("Enter another number: "))
+
+sum = first_number + second_number
+subtraction = first_number - second_number
+multiplication = first_number * second_number
+division = first_number / second_number
+
+print(sum)
+print(subtraction)
+print(multiplication)
+print(division)
