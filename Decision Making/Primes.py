3b84ea49f875d89cb354d3da51c8b220d5603915
Rami <99697671+Rami-Albaroudi@users.noreply.github.com>
2025-02-05 10:29:46 -0500

    fix

--- Decision Making/Primes.py
+++ Decision Making/Primes.py
@@ -1,16 +1,12 @@
 x = int(input("Enter a number: "))
 
-prime = True
 sqrt = int(x ** (1/2))
 
-if x == 2:
-    pass
-elif x < 2:
-    prime = False
-else:
-    for i in range(2, (sqrt + 1)):
-        if x % i == 0:
-            prime = False
+for i in range(2, (x + 1)):
+    is_prime = True
+    for y in range(2, (int(i ** (1/2)) + 1)):
+        if i % y == 0:
+            is_prime = False
             break
- 
-print(prime)
+    if is_prime == True:    
+        print(i)
